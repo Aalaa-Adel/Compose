6b7391b4409bba9681b0674e41ffbd706233b475
aalaa <aalaaadel02@gmail.com>
2024-06-27 00:35:39 +0300

    compose

--- Calculator_Task/app/src/main/java/com/example/calculatortask/MainActivity.kt
+++ Calculator_Task/app/src/main/java/com/example/calculatortask/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.example.calculatortask
 
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -20,6 +20,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -38,8 +39,10 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.calculatortask.ui.theme.CalculatorTaskTheme
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -66,7 +69,7 @@ fun App() {
             color = Color(0xFF053D57)
         ) {
             Icon(
-                painter = painterResource(id =R.drawable.sun),
+                painter = painterResource(id = R.drawable.sun),
                 contentDescription = "Sun Icon",
                 tint = Color.Black,
                 modifier = Modifier.padding(10.dp).background(Color(0xFF053D57))
